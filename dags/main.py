702c9dd4ac05f8524cadc28aac387266e4b2477d
Alien2U <166670007+Alien2U@users.noreply.github.com>
2025-10-29 14:18:10 -0700

    Initial commit.

---
+++ dags/main.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Function to run
+def hello_airflow():
+    print ("Hello from Airflow 3.0.2")
+
+# Define the DAG
+with DAG(
+    dag_id="hello-airflow_3",
+    start_date=datetime(2025, 10, 29),
+    schedule="@daily",
+    catchup=False,
+    tags=["example tag"],
+) as dag:
+    
+    t1 = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello_airflow
+    )
+
+    t1
