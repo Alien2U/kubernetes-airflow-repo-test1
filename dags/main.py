5153d283938cdbe0b097acd61473458b73d21076
Alien2U <166670007+Alien2U@users.noreply.github.com>
2025-11-06 14:17:26 -0800

    Improvement to the hello dag.

--- dags/main.py
+++ dags/main.py
@@ -1,23 +1,20 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+import logging
 
-# Function to run
-def hello_airflow():
-    print ("Hello from Airflow 3.0.2")
+def hello_airflow(**context):
+    logging.getLogger("airflow.task").info("Hello from Airflow 3.0.2")
+    return "done"
 
-# Define the DAG
-with DAG(
-    dag_id="hello-airflow_3",
-    start_date=datetime(2025, 10, 29),
-    schedule="@daily",
-    catchup=False,
-    tags=["example tag"],
-) as dag:
-    
-    t1 = PythonOperator(
+with DAG("hello_airflow_3",
+         start_date=datetime(2024,1,1),
+         schedule=None,      # manual trigger for now
+         catchup=False,
+         tags=["example"]) as dag:
+
+    PythonOperator(
         task_id="public_repo_example",
-        python_callable=hello_airflow
+        python_callable=hello_airflow,
+        retries=0,
     )
-
-    t1
