07f3e35407506d80d561bd09c26ae399706a2e50
Alien2U <166670007+Alien2U@users.noreply.github.com>
2025-12-04 12:32:29 -0800

    Wrap column names in quotes.

--- dags/sample_postgres_dag.py
+++ dags/sample_postgres_dag.py
@@ -30,7 +30,7 @@ with DAG(
         conn = hook.get_conn()
         cursor = conn.cursor()
 
-        cursor.execute("SELECT Name FROM \"rke_Account\";")
+        cursor.execute("SELECT \"Name\" FROM \"rke_Account\";")
         rows = cursor.fetchall()
 
         logger = logging.getLogger("airflow.task")
